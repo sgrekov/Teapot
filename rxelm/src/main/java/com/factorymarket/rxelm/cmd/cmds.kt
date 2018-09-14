c1bad5ddcd0fd267c07c2726ec44a35ba88378db
sngrekov <sngrekov@gmail.com>
2018-09-14 19:29:56 +0300

    add merge function for BatchCmd

--- rxelm/src/main/java/com/factorymarket/rxelm/cmd/cmds.kt
+++ rxelm/src/main/java/com/factorymarket/rxelm/cmd/cmds.kt
@@ -4,6 +4,21 @@ sealed class AbstractCmd
 open class Cmd : AbstractCmd()
 open class SwitchCmd : Cmd()
 object None : Cmd()
+
 data class BatchCmd(val cmds: MutableSet<Cmd>) : Cmd() {
     constructor(vararg commands: Cmd) : this(commands.toMutableSet())
+
+    fun merge(cmd: Cmd): BatchCmd {
+        when (cmd) {
+            is BatchCmd -> {
+                cmds.addAll(cmd.cmds)
+            }
+            is None -> {
+            }
+            else -> {
+                cmds.add(cmd)
+            }
+        }
+        return this
+    }
 }
